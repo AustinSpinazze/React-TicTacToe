df99f1901452ca0e28dfb726e23240c119b2586f
AustinSpinazze <austin.spinazze@perficient.com>
2020-02-12 14:23:40 -0600

    Made an interactive component

--- src/index.js
+++ src/index.js
@@ -3,16 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    // React components can have state by setting this.state in their constructors. this.state should be considered as 
+    // private to a React component that it’s defined in. Let’s store the current value of the Square in this.state, 
+    // and change it when the Square is clicked.
+    constructor(props) {
+        super(props);    // In JavaScript classes, you need to always call super when defining the constructor of a subclass.
+                         // All React component classes that have a constructor should start it with a super(props) call.
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
       return (
-        <button className="square">
-          {this.props.value}
+        // Added onClick that makes the button now interactive and then we call an anonmyous function inside the onClick event
+        // <button className="square" onClick={function() { alert('click'); }}>
+        // We can also save some space by using ES6 Arrow function syntax
+        // <button className="square" onClick={() => alert('click')}>
+        //   {this.props.value} */}
+        // </button>
+        //   {/* We changed this to state because used a constructor to allow our Component to have state */}
+        // --------------------------------------------------------------------------------------------------------------------
+        // By calling this.setState from an onClick handler in the Square’s render method, we tell React to re-render that Square whenever its <button> is clicked. 
+        // After the update, the Square’s this.state.value will be 'X', so we’ll see the X on the game board. If you click on any Square, an X should show up.
+        // When you call setState in a component, React automatically updates the child components inside of it too.
+        <button className="square" onClick={() => this.setState({value: 'X'})}>
+            {this.state.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    
     renderSquare(i) {
         // Here we are passing all the numbers for the squares to the Square component by using props
       return <Square value={i} />;
